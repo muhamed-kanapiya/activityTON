a425de00ad6440229cd75c4c79313f8ab43142b8
muhamed-kanapiya <muhamed08@mail.ru>
2024-10-12 22:32:45 +0500

    some changes

--- assets/tg.js
+++ assets/tg.js
@@ -1,5 +1,6 @@
 // Init TWA
 Telegram.WebApp.ready();
+Telegram.WebApp.expand();
 
 // Event occurs whenever theme settings are changed in the user's Telegram app (including switching to night mode).
 Telegram.WebApp.onEvent("themeChanged", function () {
